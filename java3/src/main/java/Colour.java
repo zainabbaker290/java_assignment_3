6e10cd32ca5ae33c595b5b6a590e81b43ecd4c29
zainabbaker290 <119340513@umail.ucc.ie>
2022-11-26 19:23:27 +0000

    added exceptions

--- java3/src/main/java/Colour.java
+++ java3/src/main/java/Colour.java
@@ -1,2 +1,20 @@
 public class Colour {
+    public double red;
+    public double green;
+    public double blue;
+
+    public Colour(double r, double g, double b){
+        if (r > 1 || r < 0){
+            throw new IllegalArgumentException("value out of range");
+        }
+        this.red = r;
+        if (g > 1 || g< 0){
+            throw new IllegalArgumentException("value out of range");
+        }
+        this.green = g;
+        if (b > 1 || b < 0){
+            throw new IllegalArgumentException("value out of range");
+        }
+        this.blue = b;
+    }
 }
--- java3/src/test/java/ColourTest.java
+++ java3/src/test/java/ColourTest.java
@@ -1,2 +1,36 @@
+import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ColourTest {
+    @Test
+    public void testDisplayRedColourTest() {
+        Colour brown = new Colour(0.59,0.0,0.0);
+        assertEquals(brown.red, 0.59, 0.59);
+
+    }
+    @Test
+    public void testDisplayGreenColourTest() {
+        Colour brown = new Colour(0.0,0.29,0.0);
+        assertEquals(brown.green, 0.29, 0.29);
+    }
+    @Test
+    public void testDisplayBlueColourTest() {
+        Colour brown = new Colour(0.0,0.0,0.0);
+        assertEquals(brown.blue, 0.0, 0.0);
+    }
+
+    @Test
+    public void exceptionRedTest(){
+        assertThrows(IllegalArgumentException.class, () -> new Colour(7,0,0));
+    }
+
+    @Test
+    public void exceptionGreenTest(){
+        assertThrows(IllegalArgumentException.class, () -> new Colour(0,5,0));
+    }
+
+    @Test
+    public void exceptionBlueTest(){
+        assertThrows(IllegalArgumentException.class, () -> new Colour(0,0,8));
+    }
 }
